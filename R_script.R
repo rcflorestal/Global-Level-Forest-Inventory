915f23a67f32fe48f316c7e354d23652bffefaa7
rcflorestal <rcflorestal@yahoo.com.br>
2020-11-04 20:39:57 -0300

    Update read.csv

--- R_script.R
+++ R_script.R
@@ -4,21 +4,21 @@ library(tidyr)
 library(stringr)
 library(ggplot2)
 
-# Load The List of Endangered Species, according to the Ordinance of the Brazilian Ministry of Environment (Portaria MMA 443/2014)
+# Read The List of Endangered Species, according to the Ordinance of the Brazilian Ministry of Environment (Portaria MMA 443/2014)
 port <- read.csv2('C:/Robson/home_office/Global-Level-Forest-Inventory/Data/attachment_443.csv',
                   header = T, comment.char = "")
 
 # Show Ordinance of the Brazilian Ministry of Environment 
 head(port)
 
-# load the forest inventory sheet
+# Read the forest inventory sheet
 poa2 <- read.csv2('C:/Robson/home_office/Global-Level-Forest-Inventory/Data/forest_inventory.csv', 
                   header = T, sep = ',', as.is = T, comment.char = "")
 
 # Show variables of forest inventory
 head(poa2)
 
-# Load Infrastructure Sheet
+# Read Infrastructure Sheet
 infra <- read.csv2('C:/Robson/home_office/Global-Level-Forest-Inventory/Data/poa2_infra.csv',
                    header = T, comment.char = "")
 
